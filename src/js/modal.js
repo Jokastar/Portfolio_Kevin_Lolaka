298e56dad06841e3aa8cb144b1715323d8d22773
Kevin Lolaka <kevinlolaka243@gmail.com>
2023-08-22 20:39:38 +0200

    first commit

---
+++ src/js/modal.js
@@ -0,0 +1,85 @@
+import { projects, techLogo } from "./projects_data";
+
+
+const grid_items = document.querySelectorAll(".portfolio-grid-item");
+const modal = document.querySelector(".modal"); 
+
+function openModal(){
+    modal.showModal();
+}
+
+function closeModal(){
+    modal.close();
+    modal.innerHTML=""; 
+}
+
+//adding event listener for all more_btn
+for(let item of grid_items){
+    item.addEventListener("click", (e)=>{
+        const p_number = e.currentTarget.id;
+        const project = projects.find(project => project.p_number == p_number); 
+
+        const modalInnerHtml = `<div class="cross"> <img src="../../public/xmark-solid.svg"/></div>
+        <div class="modal-header" id="modal-header-one">
+            <h1 id="modal-title">${project.title}</h1>
+            <span id="modal-role-desc">${project.role}</span>
+        </div>
+        <div class="modal-header">
+            <span id="modal-year">${project.year}</span>
+            <div class="tech-stack">
+                <h2>stack technique</h2>
+                <div class="tech-logos">
+                </div>
+            </div>
+        </div>
+        <div class="modal-main">
+            <div class="modal-section" id="modal-presentation">
+                <h2>presentation</h2>
+                <p>${project.presentation}</p>
+                <p>${project.mission}</p>
+            </div>
+            <div class="modal-section" id="modal-realisation">
+                <h2>realisation</h2>
+            </div>
+            <div class="links">
+                <div class="link">Voir site</div>
+                <div class="link"> Voir code</div>
+            </div>
+        </div>`
+        modal.innerHTML = modalInnerHtml;
+        
+        const techStack = document.querySelector(".tech-logos");
+        const realisation = document.getElementById("modal-realisation"); 
+        
+        for(let r of project.realisation){
+            const div = document.createElement("div"); 
+            const r_title = document.createElement("p");
+            r_title.innerText = r.fonctionality_name;
+            const r_txt = document.createElement("p");
+            r_txt.innerText = r.fonctionality_desc ; 
+            div.appendChild(r_title)
+            div.appendChild(r_txt); 
+            realisation.appendChild(div); 
+        } 
+
+        for(let tech of project.techStack){
+            let t = techLogo.find(o => o.tech === tech); 
+
+            const div = document.createElement("div"); 
+            div.classList.add("tech-logo")
+            const img = document.createElement("img"); 
+            const p = document.createElement("p"); 
+            img.src = t.img; 
+            p.innerText = t.tech
+            div.appendChild(img);
+            div.appendChild(p);
+
+            techStack.appendChild(div); 
+        }
+
+        modal.showModal(); 
+        
+    })
+}
+
+modal.addEventListener("click", closeModal); 
---
+++ src/js/projects_data.js
@@ -0,0 +1,79 @@
+function Project(p_number, title, role ,year, techStack, shortPresentation, presentation, mission,  realisation, screenshot){
+    this.p_number = p_number, 
+    this.title = title; 
+    this.role = role;
+    this.year = year; 
+    this.techStack = techStack; 
+    this.shortPresentation = shortPresentation, 
+    this.presentation = presentation;
+    this.presentation = presentation; 
+    this.mission = mission;  
+    this.realisation = realisation; 
+    this.screenshot = screenshot; 
+}
+
+const booki = new Project(
+"01",
+"Booki", 
+"intégration maquette",
+ 2023,
+ ["html", "css"],
+ "Le projet est un site Internet qui permet aux utilisateurs de trouver facilement des hébergements et des activités dans leur ville préférée.",
+  "Le projet Booki est un site Internet qui permet aux utilisateurs de trouver facilement des hébergements et des activités dans leur ville préférée.",
+  "Dans ce projet ma mission principale était de transformer les maquettes du site en une interface web fonctionnelle et esthétique.",
+  [{fonctionality_name:"responsive", fonctionality_desc:"L'un des principaux défis était de rendre le site responsive sur tous les appareils. J'ai utilisé des media queries pour ajuster les styles en fonction de la taille de l'écran, ce qui a permis de garantir une expérience utilisateur optimale."}, {fonctionality_name:"ersioning", fonctionality_desc:"Tout au long du développement, j'ai versionné le projet en utilisant Git et GitHub, ce qui a facilité la collaboration avec l'équipe et m'a permis de garder une trace de toutes les modifications apportées au code."}],
+  []
+  ); 
+
+  const portfolio = new Project(
+    "02",
+    "Portfolio Architecte", 
+    "Développement front end",
+    2023, 
+   ["html", "css", "javascript"],
+   "Site portfolio d'une architecte d'intérieur. Le projet permet à l'architecte de présenter ses travaux et d'offrir une expérience interactive à ses visiteurs",
+   "J'ai eu l'opportunité de contribuer au développement du site portfolio d'une architecte d'intérieur. Le projet permet à l'architecte de présenter ses travaux et d'offrir une expérience interactive à ses visiteurs.",
+   " Ma mission était d'intégrer de nouvelles fonctionnalités en utilisant les maquettes fournies et de créer des pages clés pour le site.",
+   [{fonctionality_name:" Ma mission était d'intégrer de nouvelles fonctionnalités en utilisant les maquettes fournies et de créer des pages clés pour le site.", fonctionality_desc:"À partir du HTML fourni, j'ai mis en place une page attrayante et réactive pour mettre en valeur les réalisations de l'architecte."},
+   {fonctionality_name:"Conception de la page de connexion de l'administrateur", fonctionality_desc:"J'ai développé cette page à partir de zéro en utilisant HTML, CSS et JavaScript pour permettre à l'architecte de se connecter et de gérer son portfolio en toute sécurité."},
+   {fonctionality_name:" Implémentation d'une modale pour uploader de nouveaux médias", fonctionality_desc:"J'ai créé cette fonctionnalité à partir de zéro, en me basant sur les besoins spécifiques de l'architecte pour ajouter facilement de nouveaux médias à son site"}
+  ],
+  []
+  ); 
+
+  const kasa = new Project(
+    "03",
+    "Kasa",
+    "Développement front end", 
+    2023,
+    ["html", "css", "react"],
+    " Le projet est un site de location d'appartements entre particuliers",
+    "J'ai eu l'opportunité de travailler sur la refonte totale de la plateforme web de Kasa,  un site de location d'appartements entre particuliers. Mon objectif était de développer l'ensemble de l'application front-end en utilisant la technologie React.",
+    "J'ai été engagé pour démarrer le projet React et développer les fonctionnalités front-end en accord avec les nouvelles maquettes et contraintes techniques.",
+    [{fonctionality_name:" Intégration des maquettes Figma", fonctionality_desc:"J'ai utilisé les maquettes fournies par le designer Paul pour concevoir l'interface de l'application en respectant la logique de composants sur Figma"},
+    {fonctionality_name:"Développement des fonctionnalités front-end", fonctionality_desc:"J'ai créé les composants React ainsi que les routes avec React Router pour offrir une expérience utilisateur fluide et responsive"},
+    {fonctionality_name:"Gestion de la galerie d'images", fonctionality_desc:"J'ai implémenté une galerie d'images avec des fonctionnalités avancées, permettant à l'utilisateur de naviguer de manière circulaire entre les photos et de profiter d'animations dynamiques"},
+    {fonctionality_name:"Collapses interactives", fonctionality_desc:"J'ai créé des Collapses interactifs qui s'ouvrent ou se ferment en fonction des actions de l'utilisateur, améliorant ainsi l'ergonomie du site."}
+     ],
+     []
+  ); 
+
+  const mon_vieux_grimoire = new Project(
+    "04",
+    "Mon Vieux Grimoire",
+    "Développement back end",
+    2023,
+    ["node", "express", "mongo"], 
+    "Le projet une plateforme de référencement et de notation de livres, permettant aux utilisateurs d'ajouter de nouveaux livres et de les noter publiquement",
+    "Le site Mon Vieux Grimoire, une plateforme de référencement et de notation de livres, permettant aux utilisateurs d'ajouter de nouveaux livres et de les noter publiquement.",
+    "J'ai été chargé de mettre en place le back-end pour gérer les données du site et les fonctionnalités de notation des livres.", 
+    [
+        {fonctionality_name:"Gestion des livres et des notations", fonctionality_desc:" J'ai créé des contrôleurs et des routeurs pour gérer les opérations CRUD (Create, Read, Update, Delete) des livres et des notations dans la base de données."}, 
+        {fonctionality_name:"Sécurité des données ", fonctionality_desc:"J'ai mis en œuvre des méthodes pour assurer la sécurité du site et de la base de données  notamment en garantissant que les adresses électroniques dans la base de données sont uniques, en utilisant la librairie json web token (JWT) pour assurer l’authentification et autorisation sur le site et enfin  en utilisant la librairie Bcrypt pour hasher les mots de passe des utilisateurs"}, 
+        {fonctionality_name:"Optimisation des images", fonctionality_desc:" j'ai optimisé les images envoyées par les utilisateurs pour réduire leur taille afin de respecter le Green Code"}
+    ],
+    []
+  ); 
+
+   export const projects = [booki, portfolio, kasa, mon_vieux_grimoire]; 
+   export const techLogo = [{tech:"html", img:"../../public/html5.svg"}, {tech:"css", img:"../../public/css3.svg"}, {tech:"javascript",img:"../../public/js.svg" }, {tech:"react", img:"../../public/react.svg"}, {tech:"node", img:"../../public/node.svg"}, {tech:"express", img:"../../public/express.svg"}, {tech:"mongo", img:"../../public/mongodb.svg"}]
